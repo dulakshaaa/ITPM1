3d1851a2783e6eab03889ea4cb977ddc108cee04
dulakshaaa <163922358+dulakshaaa@users.noreply.github.com>
2025-04-29 15:31:33 +0530

    Delete hospital-management/backend directory

--- hospital-management/backend/routes/Admin_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-// Admin login route
-router.post('/admin/login', adminController.login);
-
-module.exports = router;
